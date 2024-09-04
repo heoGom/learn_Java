43ecac54df00797fc705f2f96eb96ffe9f9d51aa
heo-gom <gjtjdwo1004@naver.com>
2024-09-04 11:02:00 +0900

    댓글 더미 데이터 입력, VIEW세팅, 쿼리문 공부

---
+++ 240903 v3/src/main/java/org/example/springv3/reply/Reply.java
@@ -0,0 +1,34 @@
+package org.example.springv3.reply;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.springv3.board.Board;
+import org.example.springv3.user.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Table(name = "reply_tb")
+@NoArgsConstructor
+@Entity
+public class Reply {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board board;
+
+    @CreationTimestamp //em.persist 할 때 발동 네이티브 쿼리에선 작동안함
+    private Timestamp createdAt;
+}

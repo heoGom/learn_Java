75211d86aaec6729c1e144e87163c16832c10f6e
heo-gom <gjtjdwo1004@naver.com>
2024-09-19 10:12:27 +0900

    v3 프로젝트 이름 변경

--- 240903 blog v3/src/main/java/org/example/springv3/user/User.java
+++ 240903 blog v3/src/main/java/org/example/springv3/user/User.java
@@ -21,12 +21,21 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username; // 아이디
     @Column(nullable = false)
-    private String password;
+    private String password;  // 해쉬 조정
     @Column(nullable = false)
     private String email;
+    // 이메일 인증 여부
+    //19세 이상 여부
 
     @CreationTimestamp
     private Timestamp createdAt;
+
+    //로그인 시도 5번 이상 틀렸는지!! 틀린횟수
+    //잠김 여부
+    //활성화 여부
+    //탈퇴여부, 탈퇴시간
+    //로그인 시에 device 장비 (window?, mac?, 휴대폰?)
+    //ip 저장
 
     @Builder
     public User(Integer id, String username, String password, String email, Timestamp createdAt) {
@@ -36,6 +45,7 @@ public class User {
         this.email = email;
         this.createdAt = createdAt;
     }
+
 
     @Override
     public String toString() {

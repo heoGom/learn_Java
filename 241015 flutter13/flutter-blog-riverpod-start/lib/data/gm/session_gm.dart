79965570d02b2c31fd037db3bd0cb2419d9b9c36
heo-gom <gjtjdwo1004@naver.com>
2024-10-15 16:09:01 +0900

    gm,자동로그인 앱과 서버

---
+++ 241015 flutter13/flutter-blog-riverpod-start/lib/data/gm/session_gm.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/_core/utils/exception_handler.dart';
+import 'package:flutter_blog/_core/utils/my_http.dart';
+import 'package:flutter_blog/data/repository/user_repository.dart';
+import 'package:flutter_blog/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
+
+//창고 겸 데이터
+class SessionGM {
+  int? id;
+  String? username;
+  String? accessToken;
+  bool isLogin;
+
+  SessionGM({this.id, this.username, this.accessToken, this.isLogin = false});
+
+  final mContext = navigatorKey.currentContext!;
+
+  Future<void> login(String username, String password) async {
+    // 1. 통신
+    var (body, accessToken) = await UserRepository().login(username, password);
+
+    Logger().d("세션 창고의 login() 메서드 실행됨 ${body}, ${accessToken}");
+
+    // 2. 성공 실패
+    if (body["success"]) {
+      Logger().d("로그인 성공");
+      // (1) SessionGM 값 변경
+      this.id = body["response"]["id"];
+      this.username = body["response"]["username"];
+      this.accessToken = accessToken;
+      this.isLogin = true;
+
+      // (2) 휴대폰 하드 저장
+      await secureStorage.write(key: "accessToken", value: accessToken);
+
+      // (3) dio에 토큰 세팅
+      dio.options.headers["Authorization"] = accessToken;
+
+      // (4) 화면 이동
+      Navigator.pushNamed(mContext, "/post/list");
+    } else {
+      Logger().d("로그인 실패");
+      ScaffoldMessenger.of(mContext).showSnackBar(
+        SnackBar(content: Text("${body["errorMessage"]}")),
+      );
+    }
+  }
+
+  Future<void> join() async {}
+
+  Future<void> logout() async {
+    await secureStorage.delete(key: "accessToken");
+    this.id = null;
+    this.username = null;
+    this.accessToken = null;
+    this.isLogin = false;
+    Navigator.popAndPushNamed(mContext, "/login");
+  }
+
+  Future<void> autoLogin() async {
+    // 1. 시큐어 스토리지에서 accessToken꺼내기
+    String? accessToken = await secureStorage.read(key: "accessToken");
+    if(accessToken==null){
+      Navigator.popAndPushNamed(mContext, "/login");
+    }else {
+      // 2. api 호출
+      Map<String, dynamic> body = await UserRepository().autoLogin(accessToken);
+
+      // 3. 세션값 갱신
+      this.id = body["response"]["id"];
+      this.username = body["response"]["username"];
+      this.accessToken = accessToken;
+      this.isLogin = true;
+
+      // 4. 정상이면 /post/list로 이동 (popand pushnamed)
+      Navigator.popAndPushNamed(mContext, "/post/list");
+    }
+  }
+}
+
+final sessionProvider = StateProvider<SessionGM>((ref) {
+  return SessionGM();
+});

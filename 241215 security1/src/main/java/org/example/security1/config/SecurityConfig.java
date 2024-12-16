50587adcaa0e45156ad73888da1deb12661e924a
heo-gom <gjtjdwo1004@naver.com>
2024-12-16 22:30:19 +0900

    oauth google 진행중

--- 241215 security1/src/main/java/org/example/security1/config/SecurityConfig.java
+++ 241215 security1/src/main/java/org/example/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.example.security1.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.security1.config.oauth.PrincipalOauth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
 // @EnableWebSecurity Spring Boot 2.1 부터 @Configuration과 SecurityFilterChain이 있으면 자동으로 Spring Security가 활성화
@@ -18,10 +21,12 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    final private PrincipalOauth2UserService principalOauth2UserService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //TODO : 로그아웃 구현 후 없애야함
-        http.csrf().disable();
+        http.csrf(csrf -> csrf.disable());
         http
                 .authorizeHttpRequests(authorize ->
                         authorize
@@ -49,6 +54,16 @@ public class SecurityConfig {
                                 .logoutSuccessUrl("/login?logout") // 로그아웃 후 이동할 URL
                                 .invalidateHttpSession(true) // 세션 무효화
                                 .deleteCookies("JSESSIONID") // 쿠키 삭제
+                )
+                // 구글 로그인
+                // 1. 코드받기(인증) 2. 엑세스 토큰(권한) 3. 사용자프로필 정보를 가져오고 4-1. 그 정보를 토대로 회원가입을 자동으로 진행
+                // 4-2. (이메일, 전화번호, 이름, 아이디) 만약 쇼핑몰이라면 -> (집주소), 백화점의 경우 (VIP등급)
+                .oauth2Login(oauth2 ->
+                        oauth2
+                                .loginPage("/loginForm")
+                                .userInfoEndpoint(userInfoEndpoint ->
+                                        userInfoEndpoint
+                                                .userService(principalOauth2UserService)) // 구글 로그인이 완료된 뒤의 후처리 필요함 Tip. 코드X,(엑세스토큰+사용자프로필정보 O)
                 );
 
 
---
+++ 241215 security1/src/main/java/org/example/security1/config/oauth/PrincipalOauth2UserService.java
@@ -0,0 +1,38 @@
+package org.example.security1.config.oauth;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        showAll(userRequest);
+
+
+        return super.loadUser(userRequest);
+    }
+
+    private void showAll(OAuth2UserRequest userRequest) {
+        System.out.println("userRequest = " + userRequest);
+        //JWT토큰의 형태인거 같다.
+        System.out.println("getAccessToken = " + userRequest.getAccessToken());
+        System.out.println("getTokenValue = " + userRequest.getAccessToken().getTokenValue());
+        System.out.println("getTokenType = " + userRequest.getAccessToken().getTokenType());
+        System.out.println("getScopes = " + userRequest.getAccessToken().getScopes());
+        System.out.println("getExpiresAt = " + userRequest.getAccessToken().getExpiresAt());
+        System.out.println("getIssuedAt = " + userRequest.getAccessToken().getIssuedAt());
+        System.out.println("getClass = " + userRequest.getAccessToken().getClass());
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("getClientRegistration = " + userRequest.getClientRegistration());
+        System.out.println("getRegistrationId = " + userRequest.getClientRegistration().getRegistrationId());
+        System.out.println("getClientId = " + userRequest.getClientRegistration().getClientId());
+        System.out.println("getClientName = " + userRequest.getClientRegistration().getClientName());
+        System.out.println("getClientSecret = " + userRequest.getClientRegistration().getClientSecret());
+        System.out.println("getRedirectUri = " + userRequest.getClientRegistration().getRedirectUri());
+    }
+}
